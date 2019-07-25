b5ba70e35dce91e3795f897df539ad82eddd363b
mentab <25363363+mentab@users.noreply.github.com>
2019-07-25 13:39:22 +0200

    draw a red rectangle oh yeah

--- index.js
+++ index.js
@@ -2,17 +2,29 @@ const w = 800;
 const h = 800;
 const m = 50;
 
-const svg = d3.select('svg')
+const projection = d3.geoMercator();
+
+const path = d3.geoPath().projection(projection);
+
+const content = d3.select('body');
+
+const svg = content.append('svg')
 	.attr('width', w)
 	.attr('height', h);
 
 Promise.all([
 	d3.json('https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json'),
 	d3.json('https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json')
-]).then((datas) => {
-	const dataCounties = datas[0];
-	const dataEducation = datas[1];
+]).then(([dataCounties, dataEducation]) => {
 
 	console.log(dataCounties);
 	console.log(dataEducation);
+
+	d3.select('svg')
+		.selectAll('path')
+		.data(topojson.feature(dataCounties, dataCounties.objects.counties).features)
+		.enter()
+		.append('path')
+		.attr('d', path)
+		.attr('fill', 'red');
 });
