f031d8a07f1be5e4635a30ed9baff4afc32de9f2
mentab <25363363+mentab@users.noreply.github.com>
2019-09-16 13:37:46 +0200

    continue project

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ const path = d3.geoPath();
 
 const content = d3.select('body');
 
+content.append('h1')
+	.attr('id', 'title')
+	.text('United States Educational Attainment');
+
+content.append('h2')
+	.attr('id', 'description')
+	.text('Percentage of adults age 25 and older with a bachelor\'s degree or higher (2010-2014)');
+
+const tooltip = content.append('div')
+	.attr('id', 'tooltip')
+	.style('position', 'absolute')
+	.style('opacity', 0);
+
 const svg = content.append('svg')
 	.attr('width', w)
 	.attr('height', h);
@@ -22,5 +35,21 @@ Promise.all([
 		.attr('d', path)
 		.attr('class', 'county')
 		.attr('data-fips', d => d.id)
-		.attr('data-education', d => 'data-education');
+		.attr('data-education', d => {
+			education = dataEducation.find(currentDataEducation =>  currentDataEducation.fips === d.id);
+			return education ? education.bachelorsOrHigher : 0;
+		})
+		.on('mouseover', d => {
+			education = dataEducation.find(currentDataEducation =>  currentDataEducation.fips === d.id);
+			console.log(education);
+			tooltip.attr('data-education', education ? education.bachelorsOrHigher : 0)
+				.style('left', 0 + 'px')
+				.style('top', 0 + 'px')
+				.style('transition', 'opacity .5s ease-out')
+				.style('opacity', .8)
+				.html(() => `Education : <em style='color:red'>${education ? education.bachelorsOrHigher : 0}</em>`);
+		})
+		.on('mouseout', function () {
+			tooltip.style('opacity', 0);
+		});;
 });
